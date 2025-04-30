d36cdfd8eee2624d5ebf6be17a550a753f155fc6
RhoSeungA <sskclara@naver.com>
2025-04-30 13:10:02 +0900

    feat: 문서 검색 시간 측정을 위해 파이프라인 일부 수정 및 난이도 설정 추가

--- router.py
+++ router.py
@@ -9,19 +9,28 @@ router = APIRouter(
 @router.get("/create")
 def makePesonalArticle(
     topic: str = Query(..., description="주제"),
-    sessionId: str = Query(..., description="세션 ID")
+    sessionId: str = Query(..., description="세션 ID"),
+    level: int = Query(..., description="글 난이도")
 ):
-    result = llm.get_ai_response(topic).get("answer", "죄송합니다. 답변을 찾을 수 없습니다.")
+    print("/create 호출")
+    result,urls,image = llm.get_ai_response(topic,level)
 
     start_index = result.find('{')
     end_index = result.rfind('}')
 
     json_string = result[start_index:end_index+1]
+    
+    json_data = {}
 
     try:
         # JSON 문자열 파싱
-        json_data = json.loads(json_string)
-        print("JSON이 정상적으로 파싱되었습니다:", json_data)
+        json_data = json.loads(json_string,strict=False)
+        
+        # url, image를 llm에서 넘겨준 것이 아닌, 검색된 기사에서 직접 받아서 사용
+        json_data['url'] = urls
+        json_data['image_url'] = image
+        
+        #print("JSON이 정상적으로 파싱되었습니다:", json_data)
         
     except json.JSONDecodeError as e:
         print("JSONDecodeError 발생:", e)
@@ -30,5 +39,7 @@ def makePesonalArticle(
     except Exception as e:
         print("알 수 없는 에러 발생:", e)
 
+    
+    
     # JSON 응답으로 반환
-    return json.loads(json_string) # FastAPI가 자동으로 JSON 변환
+    return json_data # FastAPI가 자동으로 JSON 변환
