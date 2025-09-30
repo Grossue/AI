8dfd25e27231a835d7c9aa550b9265c5d0facd91
RhoSeungA <sskclara@naver.com>
2025-09-30 11:42:35 +0900

    chore: 캐시 서버 로컬로 변경

--- redis_client.py
+++ redis_client.py
@@ -10,6 +10,8 @@ redis_port = os.getenv("REDIS_PORT")
 redis_client = redis.Redis(
     host=redis_host,
     port= int(redis_port),
-    ssl=True,
-    ssl_cert_reqs=None
+    db=0,              # 기본 DB
+    decode_responses=True  # 문자열 자동 디코딩
+    # ssl=True,
+    # ssl_cert_reqs=None
 )
